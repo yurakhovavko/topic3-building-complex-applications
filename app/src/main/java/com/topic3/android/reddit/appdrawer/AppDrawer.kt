b01615a9aba43ca21acdc4c5a084016b8dec5a5f
krosbite <killer1233215@mail.ru>
2023-05-03 23:18:25 +0300

    icon/username

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -3,11 +3,9 @@ package com.topic3.android.reddit.appdrawer
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,11 +49,39 @@ fun AppDrawer(
 @Composable
 private fun AppDrawerHeader() {
   //TODO add your code here
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Image(
+      imageVector = Icons.Filled.AccountCircle,
+      colorFilter = ColorFilter.tint(Color.LightGray),
+      modifier = Modifier
+        .padding(16.dp)
+        .size(50.dp),
+      contentScale = ContentScale.Fit,
+      alignment = Alignment.Center,
+      contentDescription = stringResource(
+        id = R.string.account
+      )
+
+    )
+    Text(
+      text = stringResource(R.string.default_username),
+      color = MaterialTheme.colors.primaryVariant
+    )
+  }
+  Divider(
+    color = MaterialTheme.colors.onSurface.copy(alpha =.2f),
+    modifier = Modifier.padding(
+      start = 16.dp, end = 16.dp, top = 16.dp
+    )
+  )
 }
 
 @Composable
 fun ProfileInfo() {
-  //TODO add your code here
+
 }
 
 @Composable
